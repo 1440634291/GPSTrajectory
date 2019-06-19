f5eaf7ce558173a7bd7a6560bd3077032a260490
1440634291 <40392839+1440634291@users.noreply.github.com>
2019-06-19 12:31:30 +0800

    GPSTrajectory
    1.0.1

--- src/cn/edu/ciec/Frame/MainFrame.java
+++ src/cn/edu/ciec/Frame/MainFrame.java
@@ -14,6 +14,26 @@ import java.awt.Choice;
 import java.awt.Color;
 import java.awt.Font;
 import javax.swing.SwingConstants;
+import javax.swing.JInternalFrame;
+import javax.swing.JToolBar;
+import javax.swing.JSplitPane;
+import javax.swing.JScrollPane;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.JLayeredPane;
+import javax.swing.JDesktopPane;
+import javax.swing.JFormattedTextField;
+import javax.swing.JTextArea;
+import javax.swing.JSpinner;
+import javax.swing.JMenuItem;
+import java.awt.Canvas;
+import java.awt.List;
+import javax.swing.JEditorPane;
+import javax.swing.JFileChooser;
+
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.awt.event.ActionEvent;
 
 public class MainFrame {
 
@@ -47,17 +67,32 @@ public class MainFrame {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 589, 461);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
 		
-		JLabel label = new JLabel("\u8BF7\u9009\u62E9\u4F60\u7684\u7167\u7247");
-		label.setHorizontalAlignment(SwingConstants.CENTER);
-		label.setFont(new Font("宋体", Font.PLAIN, 18));
-		frame.getContentPane().add(label, BorderLayout.NORTH);
+		JLabel label = new JLabel("\u6B22\u8FCE\u4F7F\u7528");
+		label.setForeground(Color.ORANGE);
+		label.setFont(new Font("华文行楷", Font.PLAIN, 40));
+		label.setBounds(210, 13, 160, 37);
+		frame.getContentPane().add(label);
 		
-		JButton button = new JButton("\u786E\u5B9A");
-		button.setHorizontalAlignment(SwingConstants.LEFT);
-		frame.getContentPane().add(button, BorderLayout.SOUTH);
+		JButton btnSubmit = new JButton("选择图片");
+		btnSubmit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser jfc = new JFileChooser();
+				jfc.setFileSelectionMode(JFileChooser.FILES_ONLY );
+				jfc.showDialog(new JLabel(), "选择");
+				File file = jfc.getSelectedFile();
+				if(file.isDirectory()){
+					System.out.println("文件夹:"+file.getAbsolutePath());
+				}else if(file.isFile()){
+					System.out.println("文件:"+file.getAbsolutePath());
+				}
+				System.out.println(jfc.getSelectedFile().getName());
+			}
+		});
+		btnSubmit.setBounds(229, 347, 113, 27);
+		frame.getContentPane().add(btnSubmit);
 	}
-
 }
